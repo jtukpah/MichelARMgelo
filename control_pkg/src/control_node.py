e401f3e8fac0ac7f43fa13b3a39a20ce35936277
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-04-18 10:13:47 -0400

    Make control_pkg nodes more robust. Add constraints.txt file and read it in both nodes. Add random trajectory mode.

--- control_pkg/src/control_node.py
+++ control_pkg/src/control_node.py
@@ -5,10 +5,10 @@
 import rospy
 from geometry_msgs.msg import Vector3
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
+import rospkg
 
 ########## CONSTRAINTS ############
-Z_MIN = 0.1
-
+constraints = None
 ###################################
 
 
@@ -18,29 +18,59 @@ def command_arm(msg):
     Command the arm to go to this point, w/in constraints.
     @param data. Vector3 message.
     """
-    x = msg.x
-    y = msg.y
-    z = max(msg.z, Z_MIN)
+    # don't start until constraints have been set.
+    while constraints is None:
+        rospy.sleep(0.05)
+    # ensure command is within constraints.
+    x = min(max(msg.x, constraints["X_MIN"]), constraints["X_MAX"])
+    y = min(max(msg.y, constraints["Y_MIN"]), constraints["Y_MAX"])
+    z = min(max(msg.z, constraints["Z_MIN"]), constraints["Z_MAX"])
     # send this command to the robot.
     bot.arm.set_ee_pose_components(x=x, y=y, z=z)
+    rospy.loginfo("Commanding ", [x,y,z])
+
+
+def read_constraints(constraints_file):
+    """
+    Read constraints from config file.
+    @param constraints_file: file object
+    """
+    global constraints
+    constraints = {}
+    lines = constraints_file.readlines()
+    for line in lines:
+        params = line.split("=")
+        constraints[params[0].strip()] = float(params[1].strip())
 
 
 def main():
     global bot
     rospy.init_node('control_node')
 
-    # define the robot object.
-    bot = InterbotixManipulatorXS("px150", "arm", "gripper")
-    # set initial end effector position.
-    bot.arm.set_ee_pose_components(x=0.3, y = 0.2, z=0.3)
-    # close the gripper.
-    bot.gripper.close()
+    try:
+        # define the robot object.
+        bot = InterbotixManipulatorXS("px150", "arm", "gripper")
+        # set initial end effector position.
+        bot.arm.set_ee_pose_components(x=0.3, y = 0.2, z=0.3)
+        # close the gripper.
+        bot.gripper.close()
+    except:
+        rospy.logerr("Unable to setup interbotix arm. Ensure the px150 arm is connected, and you have launched the drivers with\n\troslaunch interbotix_xsarm_control xsarm_control.launch robot_model:=px150\nExiting control_node.")
+        exit(0)
+
+    # find the filepath to this package.
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path('control_pkg')
+    # read constraints from file.
+    constraints_file = open(pkg_path+"/config/constraints.txt", "r")
+    read_constraints(constraints_file)
     
+    # subscribe to trajectory points.
     rospy.Subscriber("/traj/point", Vector3, command_arm)
-    rospy.loginfo("Control Node Initiated")
 
     # pump callbacks.
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
--- control_pkg/src/traj_processing_node.py
+++ control_pkg/src/traj_processing_node.py
@@ -4,39 +4,110 @@
 
 import rospy
 from geometry_msgs.msg import Vector3
+import sys
+import rospkg
+from random import uniform
+
+####### GLOBAL VARIABLES #########
+constraints = None
+traj_pt_pub = None
+##################################
 
 
-def send_trajectory(traj_file):
+def send_trajectory(traj_file, loop:bool):
     """
     Read a file, with rows in format x,y,z.
     Publish each point in the trajectory,
-    @param traj_file. file object.
+    @param traj_file: file object.
+    @param loop: bool. True will repeat the traj forever.
     """
     lines = traj_file.readlines()
-    for i in range(1, len(lines)):
-        vals = [float(v) for v in lines[i].split(",")]
-        msg = Vector3(x=vals[0], y=vals[1], z=vals[2])
-        traj_pt_pub.publish(msg)
-        # TODO may need to add a small sleep here.
-    # TODO make mode to loop through the file's trajectory to keep the arm moving.
+    r = rospy.Rate(0.5) # freq in Hz
+    while not rospy.is_shutdown():
+        for i in range(1, len(lines)):
+            vals = [float(v) for v in lines[i].split(",")]
+            msg = Vector3(x=vals[0], y=vals[1], z=vals[2])
+            traj_pt_pub.publish(msg)
+            # sleep to publish at desired freq.
+            r.sleep()
+        if not loop: return
 
-def main():
+
+def randomize_trajectory():
+    """
+    Publish a random position within constraints.
+    """
+    # don't start until constraints have been set.
+    while constraints is None:
+        rospy.sleep(0.05)
+    # loop forever, sending new random positions.
+    msg = Vector3()
+    r = rospy.Rate(0.5) # freq in Hz
+    while not rospy.is_shutdown():
+        msg.x = uniform(constraints["X_MIN"],constraints["X_MAX"])
+        msg.y = uniform(constraints["Y_MIN"],constraints["Y_MAX"])
+        msg.z = uniform(constraints["Z_MIN"],constraints["Z_MAX"])
+        traj_pt_pub.publish(msg)
+        # wait between sending points.
+        r.sleep()
+
+
+def read_constraints(constraints_file):
+    """
+    Read constraints from config file.
+    @param constraints_file: file object
+    """
+    global constraints
+    constraints = {}
+    lines = constraints_file.readlines()
+    for line in lines:
+        params = line.split("=")
+        constraints[params[0].strip()] = float(params[1].strip())
+    rospy.loginfo("Found constraints:",constraints)
+
+
+def main(mode:str):
     global traj_pt_pub
     rospy.init_node('traj_processing_node')
 
     # define publisher.
-    traj_pt_pub = rospy.Publisher('/traj/point', Vector3)
-    # TODO subscribe to Float32MultiArray of points from a different node that has generated it directly.
+    traj_pt_pub = rospy.Publisher('/traj/point', Vector3, queue_size=100)
 
-    # find a file to read a trajectory from (for now).
-    traj_file = open("trajectories/traj1.csv", "r")
-    send_trajectory(traj_file)
+    # find the filepath to this package.
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path('control_pkg')
+    # read constraints from file.
+    constraints_file = open(pkg_path+"/config/constraints.txt", "r")
+    read_constraints(constraints_file)
 
-    # # pump callbacks.
-    # rospy.spin()
+    # source of trajectory depends on cmd line arg "mode".
+    if mode == "file":
+        # find a file to read a trajectory from.
+        traj_file = open(pkg_path+"/trajectories/traj1.csv", "r")
+        send_trajectory(traj_file, loop=False)
+    elif mode == "random":
+        # keep generating random trajectory points on loop.
+        randomize_trajectory()
+    elif mode == "forward":
+        # TODO subscribe to Float32MultiArray of points from 
+        # a different node that has generated it directly, and 
+        # forward those points along one at a time.
+        print("forward mode not yet implemented")
+        exit(0)
+    else:
+        print("mode argument must be one of 'file', 'random', 'forward'.")
+        exit(0)
+    
+    # pump callbacks.
+    rospy.spin()
+
 
 if __name__ == '__main__':
+    modes = ["file","random","forward"]
     try:
-        main()
+        if len(sys.argv) > 1 and sys.argv[1] in modes:
+            main(sys.argv[1])
+        else:
+            rospy.logerr("Usage: one of\n\troslaunch control_pkg control_arm.launch mode:=MODE\n\trosrun control_pkg traj_processing_node.py MODE\nAvailable modes: ",modes)
     except rospy.ROSInterruptException:
         pass
