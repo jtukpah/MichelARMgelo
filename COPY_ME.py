5ddb30324c08e7bc800b95b1e0373b44c812e252
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-04-28 15:18:22 -0400

    Make modified trajectory gen function to draw arcs on sphere.

---
+++ COPY_ME.py
@@ -0,0 +1,58 @@
+# COPY THIS FUNCTION TO THE BOTTOM OF interbotix_ws/src/interbotix_ros_toolboxes/interbotix_xs_toolbox/interbotix_xs_modules/src/interbotix_xs_modules/arm.py.
+# ensure the indentation is correct for it to be included in the class.
+
+    def set_ee_arc_trajectory(self, x=0.2, z=0.3, roll=0, pitch=0, yaw=0, moving_time=None, wp_moving_time=0.2, wp_accel_time=0.1, wp_period=0.05):
+        print("Usage: x = change in x, z = change in z, pitch = change in pitch")
+        rpy = ang.rotationMatrixToEulerAngles(self.T_sb[:3,:3])
+        T_sy = np.identity(4)
+        T_sy[:3,:3] = ang.eulerAnglesToRotationMatrix([0.0, 0.0, rpy[2]])
+        T_yb = np.dot(mr.TransInv(T_sy), self.T_sb)
+        rpy[2] = 0.0
+        if (moving_time == None):
+            moving_time = self.moving_time
+        accel_time = self.accel_time
+        N = int(moving_time / wp_period)
+        inc = 1.0 / float(N)
+        joint_traj = JointTrajectory()
+        joint_positions = list(self.joint_commands)
+        x0 = T_yb[0][3]
+        z0 = T_yb[2][3]
+        print("Currently at ",x0 ,z0)
+        for i in range(N+1):
+            joint_traj_point = JointTrajectoryPoint()
+            joint_traj_point.positions = joint_positions
+            joint_traj_point.time_from_start = rospy.Duration.from_sec(i * wp_period)
+            joint_traj.points.append(joint_traj_point)
+            if (i == N):
+                break
+            # T_yb[:3,3] += [math.sin(i/N*math.pi/2) * x, inc * y, math.sin(i/N*math.pi/2) * z]
+            T_yb[:3,3] = [(x0+x) - x * math.cos(i/N*math.pi/2), 0, z0 + z*math.sin(i/N*math.pi/2)]
+            rpy[0] += inc * roll
+            rpy[1] += inc * pitch
+            rpy[2] += inc * yaw
+            T_yb[:3,:3] = ang.eulerAnglesToRotationMatrix(rpy)
+            T_sd = np.dot(T_sy, T_yb)
+            print("Planning to go to ", T_sd)
+            theta_list, success = self.set_ee_pose_matrix(T_sd, joint_positions, False, blocking=False)
+            if success:
+                joint_positions = theta_list
+            else:
+                rospy.loginfo("%.1f%% of trajectory successfully planned. Trajectory will not be executed." % (i/float(N) * 100))
+                break
+
+        if success:
+            self.set_trajectory_time(wp_moving_time, wp_accel_time)
+            joint_traj.joint_names = self.group_info.joint_names
+            current_positions = []
+            with self.core.js_mutex:
+                for name in joint_traj.joint_names:
+                    current_positions.append(self.core.joint_states.position[self.core.js_index_map[name]])
+            joint_traj.points[0].positions = current_positions
+            joint_traj.header.stamp = rospy.Time.now()
+            self.core.pub_traj.publish(JointTrajectoryCommand("group", self.group_name, joint_traj))
+            rospy.sleep(moving_time + wp_moving_time)
+            self.T_sb = T_sd
+            self.joint_commands = joint_positions
+            self.set_trajectory_time(moving_time, accel_time)
+
+        return success
--- control_pkg/src/control_arm.py
+++ control_pkg/src/control_arm.py
@@ -6,49 +6,61 @@
 
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 
+FREE_CONTROL = False
+
 def main():
     # define the arm object.
     bot = InterbotixManipulatorXS("px150", "arm", "gripper")
-    # set home pose.
-    # last_pos = [0.3, 0.0, 0.3, 0.0, 0.0]
-    # starting pose for circle.
-    last_pos = [0.4, 0.0, 0.1, 0.0, 0.7]
+    if FREE_CONTROL:
+        # set home pose.
+        last_pos = [0.3, 0.0, 0.3, 0.0, 0.0]
+    else:
+        # starting pose for circle.
+        last_pos = [0.4, 0.0, 0.1, 0.0, 0.7] # main arc
+        # last_pos = [0.25, 0.0, 0.15, 0.7, 0.0]
+        #last_pos = [0.21, 0.0, 0.19, 0.0, 0.0]
     bot.arm.set_ee_pose_components(x=last_pos[0], y=last_pos[1], z=last_pos[2], roll=last_pos[3], pitch=last_pos[4])
 
-    # loop and keep asking for new effort.
-    print("Enter E.E. position ('x y z') or single component (e.g. 'x 0.1').")
-    print("Possible components: x, y, z, r, p. not yaw")
-    # print(InterbotixManipulatorXS.srv_robot_info())
-    while True:
-        try:
-            line = input("Position: ").split(" ")
-            if len(line) == 3:
-                # got x y z
-                pos = [float(p) for p in line]
-            elif len(line) == 2:
-                # got e.g. x 1
-                comp = line[0]; val = float(line[1])
-                if comp == 'x':
-                    pos = [val,last_pos[1],last_pos[2],last_pos[3],last_pos[4]]
-                elif comp == 'y':
-                    pos = [last_pos[0],val,last_pos[2],last_pos[3],last_pos[4]]
-                elif comp == 'z':
-                    pos = [last_pos[0],last_pos[1],val,last_pos[3],last_pos[4]]
-                elif comp == 'r':
-                    pos = [last_pos[0],last_pos[1],last_pos[2],val,last_pos[4]]
-                elif comp == 'p':
-                    pos = [last_pos[0],last_pos[1],last_pos[2],last_pos[3],val]
+    if not FREE_CONTROL:
+        # dx = -0.18; dz = 0.105; dp = -0.8; dr = 0.0 # main arc
+        # dx = 0.0; dz = -0.0074; dp = 0.0; dr = 0.5236
+        dx = -0.19; dz = 0.0756; dp = -0.8; dr = -0.5236
+        bot.arm.set_ee_arc_trajectory(x=dx, z=dz, pitch=dp, roll=dr)
+    else:
+        # loop and keep asking for new effort.
+        print("Enter E.E. position ('x y z') or single component (e.g. 'x 0.1').")
+        print("Possible components: x, y, z, r, p. not yaw")
+        # print(InterbotixManipulatorXS.srv_robot_info())
+        while True:
+            try:
+                line = input("Position: ").split(" ")
+                if len(line) == 3:
+                    # got x y z
+                    pos = [float(p) for p in line]
+                elif len(line) == 2:
+                    # got e.g. x 1
+                    comp = line[0]; val = float(line[1])
+                    if comp == 'x':
+                        pos = [val,last_pos[1],last_pos[2],last_pos[3],last_pos[4]]
+                    elif comp == 'y':
+                        pos = [last_pos[0],val,last_pos[2],last_pos[3],last_pos[4]]
+                    elif comp == 'z':
+                        pos = [last_pos[0],last_pos[1],val,last_pos[3],last_pos[4]]
+                    elif comp == 'r':
+                        pos = [last_pos[0],last_pos[1],last_pos[2],val,last_pos[4]]
+                    elif comp == 'p':
+                        pos = [last_pos[0],last_pos[1],last_pos[2],last_pos[3],val]
+                    else:
+                        raise Exception()
                 else:
                     raise Exception()
-            else:
-                raise Exception()
-            print("Chose ", pos)
-            bot.arm.set_ee_pose_components(x=pos[0], y=pos[1], z=pos[2], roll=pos[3], pitch=pos[4])
-            # bot.arm.set_ee_cartesian_trajectory(x=pos[0], y=pos[1], z=pos[2], roll=pos[3], pitch=pos[4], moving_time=0.2, wp_moving_time=0.2, wp_period=0.05)
-            last_pos = pos
-        except:
-            print("Position must be in format 'x y z' or 'component value'")
-            continue
+                print("Chose ", pos)
+                bot.arm.set_ee_pose_components(x=pos[0], y=pos[1], z=pos[2], roll=pos[3], pitch=pos[4])
+                # bot.arm.set_ee_cartesian_trajectory(x=pos[0], y=pos[1], z=pos[2], roll=pos[3], pitch=pos[4], moving_time=0.2, wp_moving_time=0.2, wp_period=0.05)
+                last_pos = pos
+            except:
+                print("Position must be in format 'x y z' or 'component value'")
+                continue
     
 
 if __name__=='__main__':
